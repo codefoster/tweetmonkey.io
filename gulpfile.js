f4308f40ca05f95958c2235bda78ae95c46912f8
Jeremy Foster <jeremy.foster@live.com>
2015-08-12 16:11:43 -0700

    delete unnecessary files, rename edison -> tweetmonkey (pivot on projects, not options), add board options to tweetmonkey.html, misc

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 /// <reference path="typings/node/node.d.ts"/>
+
 var gulp = require('gulp');
 var app = require('./app');
-
-var lr = require('tiny-lr')();
-
-function startLivereload() {
-  lr.listen(35729);
-}
-
-// Notifies livereload of changes detected
-// by `gulp.watch()` 
-function notifyLivereload(event) {
-  lr.changed({
-    body: {
-      files: [require('path').relative(__dirname, event.path)]
-    }
-  });
-}
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('default',function(){
-	app.listen(process.env.port || 3000);
-	startLivereload();
-	gulp.watch('**/*.*', notifyLivereload);
+
 })
-//
+
+
+gulp.task('serve', function() {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['www']
+    }
+  });
+
+  gulp.watch([
+    'www/*.html',
+    'www/js/**/*.js',
+    'www/css/**/*.css',
+    'www/images/**/*',
+    'www/fonts/**/*'
+  ]).on('change', reload);
+});
